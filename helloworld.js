5986a9646b156e6cb5005d3207a3177d2edc8b22
Pravashpvsh <pokhrelpravash90@gmail.com>
2023-11-24 20:09:17 +0545

    rename file

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,6 @@
 /*
 Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-Example 1:
+Example 1: 
 Input: args = []
 Output: "Hello World"
 Explanation:
